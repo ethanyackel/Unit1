59555fdf1df026a012ce333434742b9167f24646
ethanyackel <819892@mystma.org>
2020-09-17 20:09:24 +0000

    Top Ten Movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1.Goodfellas");
+        System.out.println("2.Independence day");
+        System.out.println("3.Avengers");
+        System.out.println("4.The Revenant");
+        System.out.println("5.Scarface");
+        System.out.println("7.Cool runnings");
+        System.out.println("8.Batman");
+        System.out.println("9.Forrest gump");
+        System.out.println("10.Insidious");
+
+
+    }
+}
